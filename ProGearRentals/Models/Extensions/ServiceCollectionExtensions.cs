559af1c5b06c806f380124e6090664fc4d313ed5
Borkata03 <A@DESKTOP-QD32O4N>
2024-08-15 15:16:46 +0300

    ServiceCollectionExtensions added to simplify Program.cs

---
+++ ProGearRentals/Models/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProGearRentals.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;     
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services,IConfiguration config)
+        {
+          services
+           .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+           .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            return services;
+        }
+
+
+    }
+}

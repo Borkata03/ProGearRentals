f05b70504594cde82e926563229a508fb5b0eaaf
Borkata03 <A@DESKTOP-QD32O4N>
2024-09-10 14:37:32 +0300

    NotAnAgentAttribute

---
+++ ProGearRentals/Attributes/NotAnAgentAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ProGearRentals.Core.Contracts;
+using System.Security.Claims;
+
+namespace ProGearRentals.Attributes
+{
+    public class NotAnAgentAttribute : ActionFilterAttribute    
+    {
+       
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+
+            base.OnActionExecuted(context);
+
+            IAgentService? agentservice = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentservice == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentservice != null 
+                    && agentservice.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+
+        }
+
+
+    }
+}
